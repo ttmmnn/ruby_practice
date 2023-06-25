fad37193163604e489dcadd7fc83a188fabbb16d
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-06-26 00:28:30 +0900

    update

--- practice.rb
+++ practice.rb
@@ -4,3 +4,71 @@ puts 'ハローpaizaラーニング'
 
 # puts "勇者は、荒野を歩いていた"
 puts "モンスターがあらわれた"
+
+puts "荒野を歩いていた"
+# puts "A:モンスターがあらわれた"
+puts "すごい剣を手にいれた"
+=begin
+puts "A:ドラゴンがあらわれた"
+puts "A:魔王があらわれた"
+=end
+puts "世界を救った"
+
+player = "戦士"
+puts "#{player}は、荒野を歩いていた"
+puts "#{player}は、モンスターと戦った"
+puts "#{player}は、モンスターをたおした"
+
+player = "勇者"
+puts "#{player}はレベルアップした"
+
+player = "勇者"
+puts "#{player}の体力が回復した"
+
+team = "勇者と戦士"
+puts "#{team}の体力が回復した"
+
+player = "勇者"
+puts "#{ player }は荒野を歩いていた"
+puts "#{ player }はモンスターと戦った"
+puts "#{ player }はモンスターをたおした"
+
+number = rand(1..100)
+puts "スライムが#{number}匹あらわれた"
+
+number = rand(1..6)
+puts "サイコロの目は#{number}です"
+
+number = rand(50..99)
+puts "モンスターに、#{number}のダメージを与えた"
+
+number =1234 * 5678 / 2
+puts number
+
+a = 31
+b = 17
+puts a * b
+
+x = 8
+y = 5
+puts 8 % 5
+
+a = 1234
+b = 5678
+puts (a + b) * 3
+
+number = rand(1..10)
+money = number * 500
+puts "スライムが#{number}匹合あらわられた"
+puts "スライムを倒したときの獲得金額は#{money}円です"
+
+x = 50
+puts x - 10
+
+a = "モンスターが"
+b = "あらわれた"
+puts a+b
+
+a = "01234"
+b = "56789"
+puts a+b
