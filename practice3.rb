7dc0cb3bab1633fad04228717e99d8ded500ea63
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-06-28 23:32:49 +0900

    update

--- practice3.rb
+++ practice3.rb
@@ -91,3 +91,74 @@ for i in 1..100
     puts "<li>#{i}</li>"
 end
 puts "</ul>"
+
+# getsによる標準入力
+line = gets
+puts "hello #{line}"
+
+line = gets.to_i
+puts line * 2
+
+count = gets.to_i
+puts "データ個数 #{count}"
+for i in 1..count
+    line = gets
+    puts "hello #{line}"
+end
+
+count = gets.to_i
+for i in 1..count
+    line = gets.chomp # .chompは、末尾の改行コードを削除する
+    puts "#{line}は、スライムを攻撃した！"
+end
+
+#標準入力から整数が1つ与えられます。for inを使って、その整数回分「スライムがあらわれた」と出力する
+count = gets.to_i
+for i  in 1..count
+    puts "スライムがあらわれた"
+end
+
+# 1つ目の数値から2つ目の数値までを、1ずつ増加させながら、1行ずつ順番に出力するプログラム
+number1 = gets.to_i
+number2 = gets.to_i
+for i in number1..number2
+    puts i
+end
+
+# 入力エリアの1行目にデータの個数が整数で与えられ、2行目以降に実際のデータが与えられています。入力エリアからデータを取得して、2行目以降のデータを出力する
+number = gets.to_i
+for i in 1..number
+    name = gets
+    puts name
+end
+
+# 西暦年と平成年の対応表を作る
+# 1989年から2016年までをループで出力
+# ループ内で、各西暦年を平成年に変換
+for seireki in 1989..2016
+    print "西暦#{seireki}年は、"
+    heisei = seireki - 1988
+    puts "平成#{heisei}年です"
+end
+
+# 西暦年と昭和年の対応表を作成してください。
+# 対応表は、「西暦XXXX年は、昭和YY年です」と表示します。
+# なお、昭和年は、西暦1926年から西暦1988年までの期間で、「西暦年 - 1925」で求めることができます。
+for seireki in 1926..1988
+    print "西暦#{seireki}年は、"
+    showa = seireki - 1925
+    puts "昭和#{showa}年です。"
+end
+
+# 標準入力から、1行目に西暦年、2行目に期間が与えられます。
+# この西暦年から始まる、期間分の「西暦年と昭和年の対応表」を出力するプログラムを作成してください。
+# 対応表の各行は、「西暦XXXX年は、昭和YY年です。」と表示します。
+# 昭和年は、西暦1926年から西暦1988年までで、「西暦年 - 1925」で求めることができます。
+# なお。与えられる西暦年は、昭和年に対応しています。年数も、昭和年を超えることはありません。
+seireki = gets.to_i
+count = gets.to_i
+for i in (seireki)..(seireki + count -1)
+    print "西暦#{i}年は、"
+    showa = i - 1925
+    puts "昭和#{showa}年です。"
+end
