be34fee9023bd1d5ad0bff02101551a5a840a931
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-06-29 23:41:28 +0900

    update

---
+++ practice4.rb
@@ -0,0 +1,23 @@
+# 配列を作成する
+player_1 = "戦士"
+player_2 = "魔法使い"
+puts player_1
+puts player_2
+team = ["勇者", "魔法使い", 100, player_1]
+p team
+
+array = ["戦士","侍","僧侶","魔法使い"]
+# この下で、arrayを出力してみよう
+p array
+
+# 指定の文字を配列にする
+item = ["ロングソード", "ブレードソード", "エクスカリバー"]
+p item
+
+# 変数で、配列に代入する
+player_1 = "勇者"
+player_2 = "魔法使い"
+player_3 = "戦士"
+# player_1 ~ 3を、配列に代入して、pメソッドで出力してください。
+team = [player_1, player_2, player_3]
+p team
