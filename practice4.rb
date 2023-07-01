57f0ccf4026051b04a9dc11061fee3269329d5f3
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-07-02 00:02:47 +0900

    update

--- practice4.rb
+++ practice4.rb
@@ -157,3 +157,38 @@ while line = gets
     array.push(line)
 end
 p array
+
+# 配列を使ったランダムくじ
+# スライム,モンスター,ドラゴン,魔王
+line = gets.chomp.split(",")
+line.each do |enemy|
+	puts "#{enemy}が現れた！"
+end
+# ランダムな数を作る範囲を調べる
+num = line.length
+puts "敵は#{num}匹"
+# ランダムな数を生成
+attack = rand(num)
+# 選んだ敵に、「会心の一撃！」と表示
+puts "#{line[attack]}に会心の一撃！ #{line[attack]}を倒した！"
+
+# じゃんけんプログラム
+# 標準入力から1行取得
+line = gets.chomp
+# カンマで分割して、配列に代入
+janken = line.split(",")
+# 配列の要素数を変数に代入
+num = janken.length
+# 配列の中身を出力
+p janken
+# ランダムに選んだ配列の要素を出力
+hand = rand(num)
+puts janken[hand]
+
+# おみくじプログラム
+line = gets.chomp
+omikuji = line.split(",")
+p omikuji
+num = omikuji.length
+take = rand(num)
+puts omikuji[take]
